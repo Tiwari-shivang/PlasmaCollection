0d7c4762549972fe9ee9c0d70a1510db9a17ac32
shivang.tiwari <shubh9910tiwari@gmail.com>
2022-10-18 13:04:27 +0530

    starting from here commit

---
+++ app/src/main/java/com/example/plasmacollection/startingFromHere.java
@@ -0,0 +1,14 @@
+package com.example.plasmacollection;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class startingFromHere extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_starting_from_here);
+    }
+}
